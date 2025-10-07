6c4ab0aa8074e2145e346a6ed8ccd4b6d8e03a86
manel <manelemails@gmail.com>
2025-10-07 16:41:40 +0100

    add training pipeline

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -22,7 +22,6 @@ RandomForestClassifier,
 )
 import mlflow
 
-
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig, data_transformation_artifact:DataTransformationArtifact):
         try:
@@ -106,7 +105,7 @@ class ModelTrainer:
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=Network_Model)
 
-
+        save_object("final_model/model.pkl",best_model)
         # Model Trainer Artifact
         model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
                              train_metric_artifact=classification_train_metric,
